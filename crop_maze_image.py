4cd2212824b48701879d84fea1b15c082a79db87
triskj0 <108220065+triskj0@users.noreply.github.com>
2023-12-01 23:45:54 +0100

    fix: Correct cropping

--- crop_maze_image.py
+++ crop_maze_image.py
@@ -12,11 +12,11 @@ def crop_image(path: str) -> np.ndarray:
     # crop image
     zeros_occurences = np.where(black_white_img == 0)[0]
     top_line_index = zeros_occurences[0]
-    bottom_line_index = zeros_occurences[-1]
+    bottom_line_index = zeros_occurences[-1] + 1
 
     zeros_occurences = np.where(black_white_img[top_line_index] == 0)[0]
     l_line_index = zeros_occurences[0]
-    r_line_index = zeros_occurences[-1]
+    r_line_index = zeros_occurences[-1] + 1
 
     black_white_img = black_white_img[top_line_index:bottom_line_index, l_line_index:r_line_index]
 
