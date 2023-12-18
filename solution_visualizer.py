be7562ec9ab49401d5da2b82c748e41e9a569160
triskj0 <108220065+triskj0@users.noreply.github.com>
2023-12-18 19:52:20 +0100

    feat: Show the final result in a pop-up window

--- solution_visualizer.py
+++ solution_visualizer.py
@@ -7,6 +7,8 @@ def show_solution(image: np.ndarray, scaling_factor: int, grid: np.ndarray, shor
 
 	NUM_ROWS = len(shortest_paths) 
 	NUM_COLS = len(shortest_paths[0])
+	BLUE = np.array((255, 0, 0)).astype(np.uint8)
+
 
 	def _get_neighbours(row, col):
 		neighbours = []
@@ -24,9 +26,9 @@ def show_solution(image: np.ndarray, scaling_factor: int, grid: np.ndarray, shor
 			neighbours.append((row, col+1))
 		
 		return neighbours
-
 	
-	# 1. display the solution into grid
+
+	# display the solution into grid
 	optimal_path = [end_node]
 	current_node = end_node
 
@@ -44,14 +46,22 @@ def show_solution(image: np.ndarray, scaling_factor: int, grid: np.ndarray, shor
 		current_node = neighbours[neighbours_values.index(min(neighbours_values))]
 		optimal_path.append(current_node)
 
-	path_image = np.full((NUM_ROWS, NUM_COLS, 3), (0, 0, 0), dtype=np.int32).astype(np.uint8) # a black image
+	# only draw the path onto a black image, resizing would damage the quality of the original
+	path_image = np.full((NUM_ROWS, NUM_COLS, 3), (0, 0, 0), dtype=np.int32).astype(np.uint8)
 
 	for (r, c) in optimal_path:
-		path_image[r][c] = (255, 0, 0)
+		path_image[r][c] = BLUE
 
 	path_image = cv2.resize(path_image, (len(image[0]), len(image)))
+	image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB) # convert back to bgr to draw the path
 
 	# project it onto the original cropped image
+	for row in range(len(image)):
+		for col in range(len(image[0])):
+			if path_image[row, col, 0] > 150:
+				image[row, col] = BLUE
 
-
+	cv2.imshow('', image)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 	return
