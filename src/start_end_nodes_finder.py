dc1567c5da4066a40736ec8efb29b887f042c441
triskj0 <108220065+triskj0@users.noreply.github.com>
2023-12-28 12:02:45 +0100

    feat: Add support for same-side start&end mazes

--- src/start_end_nodes_finder.py
+++ src/start_end_nodes_finder.py
@@ -1,66 +1,78 @@
-import numpy as np
-from custom_exceptions import InvalidMazeFormatError
+from numpy import ndarray, array
 
 
-def find_start_end_nodes(grid: np.ndarray) -> tuple[int, int]:
-
+def find_start_end_nodes(grid: ndarray) -> tuple[int]:
+	
 	top_pixels = grid[0]
 	bottom_pixels = grid[-1]
-	left_pixels = np.array([grid[i][0] for i in range(len(grid))])
-	right_pixels = np.array([grid[i][-1] for i in range(len(grid))])
+	left_pixels = array([grid[i][0] for i in range(len(grid))])
+	right_pixels = array([grid[i][-1] for i in range(len(grid))])
+	last_row_index = len(grid)-1
+
+	values = {
+		id(top_pixels) : [0],
+		id(bottom_pixels) : [last_row_index],
+		id(left_pixels) : [-2, 0], # -2 is just a placeholder
+		id(right_pixels) : [-2, last_row_index]
+	}
 
 	start_node = []
 	end_node = []
 
-	if 0 in top_pixels:
-		zeros = np.where(top_pixels == 0)[0]
-		fist_0 = zeros[0]
-		last_0 = zeros[-1]
-
-		start_node.append(0)
-		start_node.append((last_0 - fist_0) // 2 + fist_0)
-
-	if 0 in bottom_pixels:
-		zeros = np.where(bottom_pixels == 0)[0]
-		fist_0 = zeros[0]
-		last_0 = zeros[-1]
-		
-		for node in (start_node, end_node):
-			if len(node) >= 2:
-				continue
-			
-			node.append(len(grid)-1)
-			node.append((last_0 - fist_0) // 2 + fist_0)
-			break
-
-	if 0 in left_pixels:
-		zeros = np.where(left_pixels == 0)[0]
-		fist_0 = zeros[0]
-		last_0 = zeros[-1]
-		
-		for node in (start_node, end_node):
-			if len(node) >= 2:
-				continue
+	first_gap_occured = False
+	second_gap_occured = False
+	start_node_first_index = -1
+	start_node_last_index = -1
+	end_node_first_index = -1
+	end_node_last_index = -1
+	first_done = False
+	second_done = False
 	
-			node.append((last_0 - fist_0) // 2 + fist_0)
-			node.append(0)
-			break
-
-	if 0 in right_pixels:
-		zeros = np.where(right_pixels == 0)[0]
-		fist_0 = zeros[0]
-		last_0 = zeros[-1]
+	for pixels in (top_pixels, bottom_pixels, left_pixels, right_pixels):
+		pixels[0], pixels[-1] = 1, 1
 		
-		for node in (start_node, end_node):
-			if len(node) >= 2:
-				continue
-
-			node.append((last_0 - fist_0) // 2 + fist_0)
-			node.append(len(grid)-1)
+		if second_gap_occured:
 			break
 
-	if not len(start_node) == 2 and len(end_node) == 2:
-		raise InvalidMazeFormatError('The maze image is of incorrect format, \
-										it should have an entry and an exit.\n')
+		for i in range(len(pixels)):
+			if pixels[i] == 0:
+				if first_gap_occured == False:
+					first_gap_occured = True
+					start_node_first_index = i
+
+				elif start_node_last_index != -1 and not second_gap_occured and not (i == start_node_first_index+1 and first_done == False):
+					second_gap_occured = True
+					end_node_first_index = i
+				else: continue
+			else:
+				if pixels[i-1] == 0:
+					if start_node_last_index == -1:
+						start_node_last_index = i-1
+					elif end_node_first_index != -1 and end_node_last_index == -1:
+						end_node_last_index = i-1
+
+		if first_done == False and first_gap_occured:
+			if start_node_last_index == -1:
+				start_node_last_index = len(pixels)-1
+			
+			start_node = values[id(pixels)].copy()
+			first_done = True
+
+			if len(start_node) == 1:
+				start_node.append((start_node_first_index+start_node_last_index)//2)
+			else: # it's either [-2, 0] or [-2, last_row_index]
+				start_node[0] = (start_node_first_index+start_node_last_index)//2
+		
+		if second_done == False and second_gap_occured:
+			if end_node_last_index == -1:
+				end_node_last_index = len(pixels)-1
+			
+			end_node = values[id(pixels)]
+			second_done = True
+
+			if len(end_node) == 1:
+				end_node.append((end_node_first_index+end_node_last_index)//2)
+			else:
+				end_node[0] = (end_node_first_index+end_node_last_index)//2
 
 	return tuple(start_node), tuple(end_node)
