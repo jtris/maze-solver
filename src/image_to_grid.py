a2b4ae04d423243d46e979fc08c97ec06c468869
triskj0 <108220065+triskj0@users.noreply.github.com>
2023-12-31 20:29:00 +0100

    perf: Optimize scaling factor thresholds

--- src/image_to_grid.py
+++ src/image_to_grid.py
@@ -3,33 +3,36 @@ import numpy as np
 
 
 def convert_img(image: np.ndarray) -> np.ndarray | tuple | int:
-	num_rows_img = len(image)
-	num_cols_img = len(image[0])
-	total_px_count = num_rows_img * num_cols_img
+    num_rows_img = len(image)
+    num_cols_img = len(image[0])
+    total_px_count = num_rows_img * num_cols_img
 
-	# scaling factor thresholds - for better performance
-	SF_1_THRESH = 13_000
-	SF_2_THRESH = 52_000
-	SF_3_THRESH = 117_000
+    # scaling factor thresholds
+    SF_1_THRESH = 10_000  # ~ 100x100
+    SF_2_THRESH = 40_000  # ~ 200x200
+    SF_3_THRESH = 100_000 # ~ 330x330 
+    SF_4_THRESH = 160_000 # ~ 400x400
 
-	if total_px_count < SF_1_THRESH:
-		scaling_factor = 1
-	elif total_px_count < SF_2_THRESH:
-		scaling_factor = 2
-	elif total_px_count < SF_3_THRESH:
-		scaling_factor = 3
-	else:
-		scaling_factor = 4
+    if total_px_count < SF_1_THRESH:
+        scaling_factor = 1
+    elif total_px_count < SF_2_THRESH:
+        scaling_factor = 2
+    elif total_px_count < SF_3_THRESH:
+        scaling_factor = 3
+    elif total_px_count < SF_4_THRESH:
+        scaling_factor = 4
+    else:
+        scaling_factor = 5
 
-	resized_image = cv2.resize(image, (num_rows_img//scaling_factor, num_cols_img//scaling_factor))
+    resized_image = cv2.resize(image, (num_rows_img//scaling_factor, num_cols_img//scaling_factor))
 
-	num_rows_resized, num_cols_resized = resized_image.shape
-	grid = []
-	for r in range(num_rows_resized):
-		grid.append([])
-		for c in range(num_cols_resized):
-			grid[r].append(0 if resized_image[r][c] == 255 else 1)
+    num_rows_resized, num_cols_resized = resized_image.shape
+    grid = []
+    for r in range(num_rows_resized):
+        grid.append([])
+        for c in range(num_cols_resized):
+            grid[r].append(0 if resized_image[r][c] == 255 else 1)
 
-	grid = np.asarray(grid)
+    grid = np.asarray(grid)
 
-	return grid, scaling_factor
+    return grid, scaling_factor
