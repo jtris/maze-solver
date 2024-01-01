675152befc3c93d0ca6ad8366c40779ce036c125
triskj0 <108220065+triskj0@users.noreply.github.com>
2024-01-01 01:52:51 +0100

    refactor: Restructure setup with setuptools.Extension

--- src/cython_setup.py
+++ src/cython_setup.py
@@ -1,9 +1,17 @@
-from setuptools import setup
+from setuptools import Extension, setup
 from Cython.Build import cythonize
 import numpy
 
 
+ext_modules = [
+    Extension(
+        'grid_dijkstras_algorithm',
+        ['grid_dijkstras_algorithm.pyx'],
+        compiler_directives={'language_level':'3'},
+        include_dirs=[numpy.get_include()]
+    )
+]
+
 setup(    
-    ext_modules=cythonize("grid_dijkstras_algorithm.pyx", compiler_directives={"language_level": "3"}),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize(ext_modules)    
 )
